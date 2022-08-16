c97d3d90a5119b0e36bb3c8669b9060cdcb876b7
Gracjan Sadowicz <gracjan@ravendb.net>
2022-08-16 16:12:59 +0200

    Initial commit

---
+++ src/ustream/chunks.py
@@ -0,0 +1,86 @@
+from __future__ import annotations
+
+import base64
+from enum import Enum
+from typing import List, Dict, Tuple
+
+CHUNK_SIZE = 256
+
+
+class UstreamChunkStatus(Enum):
+    RAW = "Raw"
+    ENCODED = "Encoded"
+
+
+class UstreamChunk:
+    def __init__(
+            self,
+            chunk: bytes,
+            part_number: int,
+            status: UstreamChunkStatus = UstreamChunkStatus.RAW,
+            length: int = CHUNK_SIZE,
+    ):
+        self.data = chunk
+        self.part_number = part_number
+        self.status = status
+        self.length = length
+
+    def to_json(self) -> Dict:
+        return {
+            "data_chunk": self.data,
+            "part_number": self.part_number,
+            "status": self.status.value,
+            "length": self.length,
+        }
+
+    @classmethod
+    def from_json(cls, json_dict) -> UstreamChunk:
+        return cls(
+            json_dict["data_chunk"],
+            json_dict["part_number"],
+            UstreamChunkStatus(json_dict["status"]),
+            json_dict["length"],
+        )
+
+
+def add_extra_bytes_if_needed(short_bytes: bytes) -> Tuple[bytes, int]:
+    length = len(short_bytes)
+    if length < CHUNK_SIZE:
+        short_bytes += b"0" * (CHUNK_SIZE - len(short_bytes))
+    return short_bytes, length
+
+
+def remove_extra_bytes_if_needed(data_chunk: bytes, true_length: int) -> bytes:
+    if len(data_chunk) == true_length:
+        return data_chunk
+    return data_chunk[:true_length]
+
+
+def bytes_to_chunks(data: bytes) -> List[UstreamChunk]:
+    chunks = []
+    # chopping the 'data' on CHUNK_SIZE'd parts
+    for i in range(int(len(data) / CHUNK_SIZE)):
+        cursor = i * CHUNK_SIZE
+        next_chunk = data[cursor: cursor + CHUNK_SIZE]
+        chunks.append(UstreamChunk(next_chunk, i))
+
+    # final chop
+    last_chunk_start = len(chunks) * CHUNK_SIZE
+    last_chunk = data[last_chunk_start: last_chunk_start + CHUNK_SIZE]
+
+    # if last chunk length isn't equal to the standards - make it equal to the standards
+    last_chunk, length = add_extra_bytes_if_needed(last_chunk)
+
+    chunks.append(UstreamChunk(last_chunk, len(chunks), length=length))
+
+    return chunks
+
+
+def chunks_to_bytes(chunks: List[UstreamChunk]) -> bytes:
+    chunks.sort(key=lambda x: x.part_number)
+    byte_chunks = [chunk.data for chunk in chunks[:-1]]
+    body = base64.b64decode(b''.join(byte_chunks))
+
+    tail_decoded = base64.b64decode(chunks[-1].data)
+    tail = remove_extra_bytes_if_needed(tail_decoded, chunks[-1].length)
+    return body + tail
---
+++ src/ustream/server/server.py
@@ -0,0 +1,63 @@
+import base64
+from typing import Dict
+
+from aiohttp import web
+import socketio
+import hashlib
+
+from src.ustream.chunks import UstreamChunk, UstreamChunkStatus
+
+sio = socketio.AsyncServer()
+app = web.Application()
+sio.attach(app)
+
+
+class Session:
+    def __init__(self, sid: str):
+        self.sid = sid
+        self.chunks_parsed: int = 0
+
+
+def encode_h264(data: bytes) -> bytes:
+    encoded_data = data  # todo: h.264 https://github.com/opencv/opencv-python/issues/100#issuecomment-554870068
+    return encoded_data
+
+
+def encrypt_sha256(data: bytes) -> str:
+    return hashlib.sha256(data).hexdigest()
+
+
+def encode_data_h264_b64(data_chunk: bytes) -> bytes:
+    data_chunk = encode_h264(data_chunk)
+    data_chunk = base64.b64encode(data_chunk)
+    return data_chunk
+
+
+@sio.on("session_create")
+async def create_session(sid):
+    return f"Created new session of id '{sid}'"
+
+
+@sio.on("session_process")
+def session_process(sid, data: Dict):
+    ustream_chunk = UstreamChunk.from_json(data)
+
+    print("Processing data...")
+    processed_data = encode_data_h264_b64(ustream_chunk.data)
+    ustream_chunk.data = processed_data
+    ustream_chunk.status = UstreamChunkStatus.ENCODED
+
+    print(f"Returning {len(processed_data)} bytes of h.264 & b64 encoded data.")
+    return ustream_chunk.to_json()
+
+
+@sio.on("session_close")
+def disconnect(sid):
+    return f"Session of id '{sid}' has been closed"
+
+
+def run_server():
+    web.run_app(app, port=2137)
+
+if __name__ == "__main__":
+    run_server()
